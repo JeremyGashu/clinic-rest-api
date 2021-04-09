fe584564d107ca379d4cf866fd282957dd48716b
JeremyGashu <gearmias@gmail.com>
2021-04-09 22:13:38 +0300

    addes cors

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 //ROUTES
 app.use('/api/admin', adminRoute)
 app.use('/api/clinics', clinicRoute)
